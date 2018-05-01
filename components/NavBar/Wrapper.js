f5befdda0d3940638957fccb4aa9fb038496451b
khaja <khazamastan@gmail.com>
2018-05-01 10:22:44 +0530

    shdow

--- components/NavBar/Wrapper.js
+++ components/NavBar/Wrapper.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 const Wrapper = styled.nav`
     padding: 10px 15px;
     background: #333;
+    box-shadow: 0px 0px 1px 1px #403d3d;
     ul{
         margin : 0;
         li{
