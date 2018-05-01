eba32ef6e7f8f04c58a303245e259db3effb97a8
khaja <khazamastan@gmail.com>
2018-05-01 10:27:03 +0530

    dummy movie image

--- components/MovieImage/index.js
+++ components/MovieImage/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 const MovieImage = props => {
 	const {imageURL, imageAlt} = props;
-	if (typeof imageURL === 'string') {
+	if (typeof imageURL === 'string' && imageURL != "N/A") {
 		return <img src={imageURL} alt={imageAlt}/>;	
 	}
-	return <img src='' alt="placeholder"/>;
+	return <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDzIzPMFcfjaa0NI2qfdBdx3l_6jQvqCjbn_tJul8oVKqOZf1D6w' alt="placeholder"/>;
 };
 MovieImage.propTypes = {
 	imageURL: PropTypes.string,
